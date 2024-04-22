6b47675d3e75b3ab0036032372fdcbf5307c153d
Enrique <enrique.sousa@gmail.com>
2024-04-21 19:31:15 -0700

    03. Traducir nuestra aplicación de Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TestLaravel11Jet</title>
+        <title>Welcome | Basic Start kit</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
